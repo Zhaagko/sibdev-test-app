4261ef48de2db3e33a7b4c377aea79d431cefc9a
bogdangilmutdinov <gilmutdinov.bogdazoe@gmail.com>
2023-09-12 18:17:24 +0200

    Add Deal model and make migration.

--- deals/models.py
+++ deals/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Deal(models.Model):
+    customer = models.CharField(max_length=512)
+    item = models.CharField(max_length=512)
+    total = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    date = models.DateTimeField()
